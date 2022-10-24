63865434211f78252ea6433cdf38e66a7493134e
Wesley <wesley.medeiros6938@gmail.com>
2022-10-24 11:35:26 -0300

    Finalizando projeto

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,22 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
 import Avatar from './Avatar'
 import styles from './Comment.module.css'
 
-function Comment(props) {
+function Comment({content, onDeleteComment}) {
+  const [likedCount, setLikedCount] = useState(0)
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikedCount(likedCount + 1)
+  }
+
   return(
     <div className={styles.comment}>
-      <Avatar  src="https://github.com/Wesley-Medeiros.png" />
+      <Avatar hasBorder= {false}  src="https://github.com/Wesley-Medeiros.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
@@ -15,19 +26,19 @@ function Comment(props) {
             <time title='22 de setembro às 08:13h' dateTime="2022-25-02 08:00:00">Cerca de 2h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
             <Trash size={24}/>
             </button>
             
             
           </header>
 
-            <p>{props.content}</p>
+            <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
           <ThumbsUp size={20}/>
-          Aplaudir <span>20</span>
+          Aplaudir <span>{likedCount}</span>
           </button>
 
         </footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,6 +14,7 @@ function Post({ author, publishedAt, content}) {
   ])
 
   const [newCommentText, setNewCommentText] = useState("")
+  
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -33,7 +34,22 @@ function Post({ author, publishedAt, content}) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity("")
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esse campo é obrigatório!")
+  }
+
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne =  comments.filter(comment => {
+      return comment !== commentToDelete;
+    })
+
+    setComments(commentsWithoutDeletedOne);
+
   }
 
 
@@ -58,9 +74,9 @@ function Post({ author, publishedAt, content}) {
       <div className={styles.content}>        
        {content.map(line => {
          if ( line.type === 'paragraph') {
-           return(<p>{line.content}</p>);
+           return(<p key ={line.content}></p>);
           }else if (line.type === 'link') {
-           return(<p> <a href="#">{line.content}</a></p>);
+           return(<p key={line.content}><a href="#">{line.content}</a></p>);
           
           }
           
@@ -75,17 +91,20 @@ function Post({ author, publishedAt, content}) {
         placeholder='Deixe seu comentário'
         value = {newCommentText}
         onChange = {handleNewCommentChange} 
+        onInvalid = {handleNewCommentInvalid}
+        required
         />
 
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type='submit'disabled={newCommentText.lenght === 0}>Publicar</button>
         </footer>
       </form>
       <div className={styles.commentList}>
        {comments.map(comment => {
-         return <Comment 
+         return <Comment key = {comment}
          content={comment}
+         onDeleteComment={deleteComment}
          />
        })}
      </div>
